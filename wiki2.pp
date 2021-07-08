98a945f8a96f64aac0581b900b72447f55ece644
pmn-dso <throwayaw@pm.me>
2021-07-08 09:47:53 +0000

    wiki2

--- wiki2.pp
+++ wiki2.pp
@@ -46,62 +46,78 @@ exec { 'deplacement de dokuwiki':
 # Création vhost apache2
 
 file { '/var/www/politique':
-  before => Exec['copie-dokuwiki'],
-  ensure => 'directory',
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
+  #before  => Exec['copie-dokuwiki'],
+  ensure  => 'directory',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  source  => '/usr/src/dokuwiki/',
+  path    => '/var/www/politique',
+  recurse => true;
 }
 
 file { '/var/www/recettes':
-  before => Exec['copie-dokuwiki'],
-  ensure => 'directory',
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
+  #before  => Exec['copie-dokuwiki'],
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  source  => '/usr/src/dokuwiki/',
+  path    => '/var/www/recettes',
+  recurse => true;
 }
 
 # Copie de dokuwiki dans les vhosts correspondant
 
-exec { 'copie-dokuwiki':
-    before  => Exec['droits-dokuwiki'],
-    cwd     => '/usr/src/',
-    command => 'rsync -a dokuwiki/ /var/www/politique && rsync -a dokuwiki/ /var/www/recettes',
-    path    => ['/usr/bin', '/usr/sbin',],
-}
+#exec { 'copie-dokuwiki':
+#    before  => Exec['droits-dokuwiki'],
+#    cwd     => '/usr/src/',
+#    command => 'rsync -a dokuwiki/ /var/www/politique && rsync -a dokuwiki/ /var/www/recettes',
+#    path    => ['/usr/bin', '/usr/sbin',],
+#}
 
 # Modification user:group
-exec { 'droits-dokuwiki':
-    command => 'chown -R www-data:www-data /var/www/politique && chown -R www-data:www-data /var/www/politique',
-    path    => ['/usr/bin', '/usr/sbin',],
-}
+#exec { 'droits-dokuwiki':
+#    command => 'chown -R www-data:www-data /var/www/politique && chown -R www-data:www-data /var/www/politique',
+#    path    => ['/usr/bin', '/usr/sbin',],
+#}
 
 # Config vhost
 
-exec { 'copie-conf-vhost':
-    command => 'cp /etc/apache2/sites-available/000-default.conf /var/www/politique/politique.conf && cp /etc/apache2/sites-available/000-default.conf /var/www/recettes/recettes.conf',
-    path    => ['/usr/bin', '/usr/sbin',],
+file { 'copie-conf-vhost-politique':
+  before  => Exec['conf-vhost'],
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  source  => '/etc/apache2/sites-available/000-default.conf',
+  path    => '/var/www/politique/politique.conf';
+  
+  'copie-conf-vhost-recettes':
+    before  => Exec['conf-vhost'],
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    source  => '/etc/apache2/sites-available/000-default.conf',
+    path    => '/var/www/recettes/recettes.conf';
 }
 
 exec { 'conf-vhost':
-    command => 'sed -i \'s/html/politique/g\' /var/www/politique/politique.conf && sed -i \'s/html/recettes/g\' /var/www/recettes/recettes.conf',
-    path    => ['/usr/bin', '/usr/sbin',],
+  command => 'sed -i \'s/html/politique/g\' /var/www/politique/politique.conf && sed -i \'s/#ServerName www.example.com/ServerName politique.wiki/g\' /var/www/politique/politique.conf && sed -i \'s/html/recettes/g\' /var/www/recettes/recettes.conf && sed -i \'s/#ServerName www.example.com/ServerName recettes.wiki/g\' /var/www/recettes/recettes.conf',
+  path    => ['/usr/bin', '/usr/sbin',];
 }
-
-exec { 'port-conf-vhost':
-    command => 'sed -i \'s/*:80/*:1080/g\' /var/www/politique/politique.conf && sed -i \'s/*:80/*:1080/g\' /var/www/recettes/recettes.conf',
-    path    => ['/usr/bin', '/usr/sbin',],
-}
-
 
 exec { 'link-vhost':
-    command => 'ln -s /var/www/politique/politique.conf /etc/apache2/sites-available/politique.conf && ln -s /var/www/recettes/recettes.conf /etc/apache2/sites-available/recettes.conf',
-    path    => ['/usr/bin', '/usr/sbin',],
+  command => 'ln -s /var/www/politique/politique.conf /etc/apache2/sites-available/politique.conf && ln -s /var/www/recettes/recettes.conf /etc/apache2/sites-available/recettes.conf',
+  path    => ['/usr/bin', '/usr/sbin',];
 }
 
 
 exec { 'activation-vhost':
-    command => 'a2ensite recettes && a2ensite politique',
-    path    => ['/usr/bin', '/usr/sbin',],
+  command => 'a2ensite recettes && a2ensite politique',
+  path    => ['/usr/bin', '/usr/sbin',],
+  notify => Service[apache2]
 }
 
+service { 'apache2':
+  ensure => running;
+}
