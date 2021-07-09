6bbc2d23bf84fdcf42eccecb561a8c7efcd5635c
pmn-dso <throwayaw@pm.me>
2021-07-09 11:51:57 +0000

    Add templates

--- wiki-correction.pp
+++ wiki-correction.pp
@@ -15,18 +15,21 @@ class dokuwiki::hosting {
   }
 }
 
+# Les parametres réutilisables
 class dokuwiki::params {
-  $src_dir = "/usr/src" 
-  $dokuwiki_archive = "${src_dir}/dokuwiki.tgz" 
-  $dokuwiki_dir = "${src_dir}/dokuwiki-2020-07-29" 
+  $src_dir = '/usr/src'
+  $dokuwiki_archive = "${src_dir}/dokuwiki.tgz"
+  $dokuwiki_dir = "${src_dir}/dokuwiki-2020-07-29"
 }
 
+# Les éléments génériques
 class dokuwiki {
   include dokuwiki::params
   include dokuwiki::source
   include dokuwiki::hosting
 }
 
+# L'installation de dokuwiki depuis les sources
 class dokuwiki::source {
   include dokuwiki::params
 
@@ -38,14 +41,15 @@ class dokuwiki::source {
 
   exec {
     'dokuwiki::unarchive':
-      cwd     => "${src_dir}",
+      cwd     => $src_dir,
       command => "tar xavf ${dokuwiki_archive}",
-      creates => "${dokuwiki_dir}",
+      creates => $dokuwiki_dir,
       path    => ['/bin'],
-      require => File["${dokuwiki_archive}"],
+      require => File[$dokuwiki_archive];
   }
 }
 
+# Une ressource permettant de déploiement d'un site spécifique
 define dokuwiki::site (String $site_name, String $site_dns) {
   include dokuwiki::params
 
@@ -55,12 +59,12 @@ define dokuwiki::site (String $site_name, String $site_dns) {
       owner   => 'www-data',
       group   => 'www-data',
       # mode    => '0755',
-      source  => "${dokuwiki_dir}",
+      source  => $dokuwiki_dir,
       recurse => true;
 
     "/etc/apache2/sites-available/${site_name}.conf":
       ensure  => present,
-      content => template('/home/vagrant/teaching-devops-puppet-v20210628/templates/site.conf.erb'),
+      content => template('/home/vagrant/071ca2227754705837aa3ef9748ed59e9f8a015fd765c42f391a4cbc271c6d5e/templates/site.conf.erb'),
       require => [Package['apache2'],
       File["/var/www/${site_name}"]];
 
@@ -76,7 +80,7 @@ define dokuwiki::site (String $site_name, String $site_dns) {
   }
 
   host {
-    "${site_dns}":
+    $site_dns:
       ip => '127.0.0.1';
   }
 }
@@ -88,13 +92,13 @@ node 'server0' {
   include dokuwiki       # les trucs génériques de dokuwiki
 
   dokuwiki::site {
-    "siteA":
-      site_name => "politique-wiki",
-      site_dns  => "politique.wiki";
+    'siteA':
+      site_name => 'politique-wiki',
+      site_dns  => 'politique.wiki';
 
-    "siteB":
-      site_name =>  "tajineworld-com",
-      site_dns  => "tajineworld.com";
+    'siteB':
+      site_name => 'tajineworld-com',
+      site_dns  => 'tajineworld.com';
   }
 }
 
@@ -102,8 +106,8 @@ node 'server1' {
   include dokuwiki
 
   dokuwiki::site {
-    "recettes":
-      site_name => "recettes-wiki",
-      site_dns  => "recettes.wiki";
+    'recettes':
+      site_name => 'recettes-wiki',
+      site_dns  => 'recettes.wiki';
   }
 }
