760e172fdd558f223b2e1379f5c48732f8bca1a4
pmn-dso <throwayaw@pm.me>
2021-07-09 07:53:07 +0000

    Add wiki

--- wiki.pp
+++ wiki.pp
@@ -1,43 +1,129 @@
-# Étape 2
-package {
 
-  'php7.3':
-    ensure => 'present',
-    name   => 'php7.3';
-
-  'apache2':
-    ensure => 'present';
+class development {
+  package {
+    'vim':
+      ensure =>  installed;
+    'make':
+      ensure =>  installed;
+    'gcc':
+      ensure =>  installed;
+  }
 }
 
-# Étape 3
+class hosting {
+  package {
+    'apache2':
+      ensure =>  installed;
+    'php7.3':
+      ensure =>  installed;
+  }
+}
 
-file { 
-  '/usr/src/dokuwiki.tgz':
+class dokuwiki {
+  file { '/usr/src/dokuwiki.tgz':
     ensure         => 'present',
     source         => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
     path           => '/usr/src/dokuwiki.tgz',
     checksum_value => '8867b6a5d71ecb5203402fe5e8fa18c9',
-}
+  }
 
-#exec { 
-#  'wget-dokuwiki':
-#    cwd     => '/usr/src',
-#    command => '/usr/bin/wget -O /usr/src/dokuwiki.tgz https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz';
-#}
-
-# Étape 4
-
-exec { 
-  'Extraction-dokuwiki':
+  exec { 'extraction-dokuwiki':
+    require => File['/usr/src/dokuwiki.tgz'],
     cwd     => '/usr/src',
     command => 'tar -xzvf dokuwiki.tgz',
     creates => '/usr/src/dokuwiki',
     path    => ['/usr/bin', '/usr/sbin',],
+  }
+
+  exec { 'deplacement de dokuwiki':
+    require => Exec['extraction-dokuwiki'],
+    cwd     => '/usr/src/',
+    ommand => 'mv dokuwiki-2020-07-29/ dokuwiki',
+    path    => ['/usr/bin', '/usr/sbin',],
+  }
 }
 
-file {
-  'deplacement de dokuwiki':
-    ensure => 'present',
-    source => '/usr/src/dokuwiki-2020-07-29',
-    path   => '/usr/src/dokuwiki';
+class politique {
+  file { '/var/www/politique':
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0755',
+    source  => '/usr/src/dokuwiki/',
+    path    => '/var/www/politique',
+    recurse => true;
+  }
+  file { 'copie-conf-vhost-politique':
+    before  => Exec['conf-vhost'],
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    source  => '/etc/apache2/sites-available/000-default.conf',
+    path    => '/var/www/politique/politique.conf';
+  }
+  exec { 'activation-vhost-politique':
+    require => Exec['link-vhost'],
+    command => 'a2ensite politique',
+    path    => ['/usr/bin', '/usr/sbin',],
+    notify => Service['apache2']
+  }
 }
+
+class vhost {
+  exec { 'conf-vhost':
+    command => 'sed -i \'s/html/politique/g\' /var/www/politique/politique.conf && sed -i \'s/#ServerName www.example.com/ServerName politique.wiki/g\' /var/www/politique/politique.conf && sed -i \'s/html/recettes/g\' /var/www/recettes/recettes.conf && sed -i \'s/#ServerName www.example.com/ServerName recettes.wiki/g\' /var/www/recettes/recettes.conf',
+    path    => ['/usr/bin', '/usr/sbin',];
+  }
+
+  exec { 'link-vhost':
+    require => Exec['conf-vhost'],
+    command => 'ln -s /var/www/politique/politique.conf /etc/apache2/sites-available/politique.conf && ln -s /var/www/recettes/recettes.conf /etc/apache2/sites-available/recettes.conf',
+    path    => ['/usr/bin', '/usr/sbin',];
+  }
+  service { 'apache2':
+    ensure => running;
+  }
+}
+
+class recettes {
+  file { '/var/www/recettes':
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0755',
+    source  => '/usr/src/dokuwiki/',
+    path    => '/var/www/recettes',
+    recurse => true;
+  }
+  
+  file { 'copie-conf-vhost-recettes':
+    before  => Exec['conf-vhost'],
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    source  => '/etc/apache2/sites-available/000-default.conf',
+    path    => '/var/www/recettes/recettes.conf';
+  }
+
+  exec { 'activation-vhost-recettes':
+    require => Exec['link-vhost'],
+    command => 'a2ensite recettes',
+    path    => ['/usr/bin', '/usr/sbin',],
+    notify => Service['apache2']
+  }
+}
+
+node 'control' {
+  include development
+}
+
+node 'server0' {
+  include hosting
+  include dokuwiki
+}
+
+node 'server1' {
+  include hosting
+  include dokuwiki
+}
+
